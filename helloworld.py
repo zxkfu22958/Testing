8b92ea81f4246b0f3232aae7beab17a2fe05fa3c
warren <mahebrahvis@gmail.com>
2022-06-25 18:50:22 +0800

    hi

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+print("hello world")
+# interpretation
+name = 'keyer'
+age = 28
+new_str = 'I am ' + name + ',' + 'i am' + str(age) + '.'
+
+print(new_str)
+
+new_str_1 = "i am {name}, i am {age} years old." .format(
+    name = 'kayer', age = '28'
+)
+print(new_str_1)
+
+name1 = "K"
+age2 = 99
+
+new_str_2 = f'i am {name1}, i am {age2} years old.'
+print(new_str_2)
+
+
+
+
+
